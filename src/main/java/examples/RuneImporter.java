2534b7c5cc371af63f3b1705d4529294f95f166d
Gergö Kadar <kadargergo20@gmail.com>
2024-01-29 13:32:58 +0100

    er ved at have fat i noget

--- src/main/java/examples/RuneImporter.java
+++ src/main/java/examples/RuneImporter.java
@@ -3,7 +3,12 @@ package examples;
 import com.stirante.lolclient.ClientApi;
 import com.stirante.lolclient.ClientConnectionListener;
 import generated.LolPerksInventoryRunePageCount;
+import generated.LolPerksPerkPageResource;
 import generated.LolSummonerSummoner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RuneImporter {
 
@@ -18,8 +23,26 @@ public class RuneImporter {
                         return;
                     }
                     LolSummonerSummoner summoner = api.executeGet("/lol-summoner/v1/current-summoner", LolSummonerSummoner.class).getResponseObject();
-                    LolPerksInventoryRunePageCount runepageNumber = api.executeGet("/lol-perks/v1/currentpage", LolPerksInventoryRunePageCount.class).getResponseObject();
-                    System.out.println(runepageNumber.);
+                    LolPerksPerkPageResource runepageNumber = api.executeGet("/lol-perks/v1/currentpage",
+                            LolPerksPerkPageResource.class).getResponseObject();
+                    if(!runepageNumber.isEditable || !runepageNumber.isActive)
+                    {
+                        LolPerksPerkPageResource[] allPages = api.executeGet("/lol-perks/v1/pages",
+                                LolPerksPerkPageResource[].class).getResponseObject();
+
+                        List<LolPerksPerkPageResource> availablePages = Arrays.stream(allPages).filter(p -> p.isEditable).collect(Collectors.toList());
+                        if(availablePages.size() > 0)
+                        {
+                            runepageNumber = availablePages.get(0);
+                        }
+
+                        else
+                        {
+                            runepageNumber = new LolPerksPerkPageResource();
+                        }
+                    }
+
+                    page.
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
